ee3dfed708336d51884c60e376a9921f4fb79e3f
mhdabdellah <86131274+mhdabdellah@users.noreply.github.com>
2025-04-24 22:05:37 +0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8080/api/";
-const API_BASE_URL = "http://127.0.0.1:8080/api/";
-// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://backend-xq1d.onrender.com/api/";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://backend-xq1d.onrender.com/api/";
 
 
 const apiClient  = axios.create({
